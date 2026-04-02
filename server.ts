fc908e342c5c59bdbdf5c42afd91c305c4ad968b
specialcowboy69 <franciscoestangui@yahoo.es>
2026-04-02 12:41:59 +0200

    Prepare app for secure deployment on Render

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ dotenv.config({ path: ".env.local" });
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.disable("x-powered-by");
 app.use(express.json({ limit: "16kb" }));
